a2a7758cfba66ea7e96576e6c18514d1613bfc56
Eng. Yazeed Ali <engyazeedali@gmail.com>
2024-01-26 22:42:06 +0300

    cors middleware

--- app/main.py
+++ app/main.py
@@ -2,11 +2,26 @@ from fastapi import FastAPI
 from app.api.user_api import user_router
 from app.api.attendance_api import attendance_router
 from app.services.user_services import create_admin_user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 #______________________________________________________________________________
+#CORS Middleware
+origins = [
+    "http://localhost:3000"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods=["GET", "PUT", "POST", "DELETE"],
+    allow_headers=["Content-Type"],
+)
+#______________________________________________________________________________
+
+#Root Route
 @app.get('/')
 def read_root():
     return "This is The Royal Test FAST API Project. You can find the API Documentation at /docs"
